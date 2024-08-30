30219d3b5a985b01ecc5212b7d6b7077531c10f7
Divineleo2001 <divineleo20@gmail.com>
2024-08-30 16:02:40 +0530

    change

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -6,7 +6,6 @@ const Spinner = () => {
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
         Loading...
-        TO Intern
       </span>
     </div>
   );
